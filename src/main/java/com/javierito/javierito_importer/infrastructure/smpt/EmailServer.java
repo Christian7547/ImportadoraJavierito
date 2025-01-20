83c31b5ed80476e3fbc5d006d1341056ebf86d4c
JayLoKing <mendezpabel41@gmail.com>
2025-01-19 23:13:25 -0400

    Update smpt

--- src/main/java/com/javierito/javierito_importer/infrastructure/smpt/EmailServer.java
+++ src/main/java/com/javierito/javierito_importer/infrastructure/smpt/EmailServer.java
@@ -17,6 +17,7 @@ public class EmailServer implements IEmailServer {
 
     public void sendEmail(String to, String subject, String body) {
         MimeMessage mimeMessage = mailSender.createMimeMessage();
+
         try {
             MimeMessageHelper helper = new MimeMessageHelper(mimeMessage, true);
             helper.setTo(to);
