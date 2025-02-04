75ef6dcdbb5a9c06e23d4509e7fd173fe70c1ee3
Christian7547 <eliasgonzalesec@gmail.com>
2025-02-04 01:26:09 -0400

    Merge cereza into dev

---
+++ src/main/java/com/javierito/javierito_importer/application/Utils/Generator.java
@@ -0,0 +1,19 @@
+package com.javierito.javierito_importer.application.services.Utils;
+
+import java.util.Random;
+
+public class Generator {
+
+    private static final String characters = "ABCDEFGHIJKLMNOPQRSTUVWXYZabcdefghijklmnopqrstuvwxyz0123456789";
+
+    public static String GenerateRecoveryCode(){
+        StringBuilder recoveryCode = new StringBuilder();
+        Random random = new Random();
+
+        for (int i = 0; i < 5; i++) {
+            int index = random.nextInt(characters.length());
+            recoveryCode.append(characters.charAt(index));
+        }
+        return recoveryCode.toString();
+    }
+}
