e81e20e7315476974993013cd24e089f952ebaac
YCerezaY <101646097+Furinyan-Gi@users.noreply.github.com>
2025-03-03 15:09:25 -0400

    fixed delete item

--- src/main/java/com/javierito/javierito_importer/domain/models/Item.java
+++ src/main/java/com/javierito/javierito_importer/domain/models/Item.java
@@ -20,12 +20,12 @@ public class Item {
     private BigDecimal barePrice;
     private int brandID;
     private short subCategoryID;
-    private BigDecimal weight;
     private String dateManufacture;
     private short status;
     private LocalDateTime registerDate;
     private LocalDateTime  lastUpdate;
     private short  itemAddressID;
     private long userID;
+    private String acronym;
 
 }
