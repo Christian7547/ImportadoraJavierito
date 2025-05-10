253836194932a032350a30df69e9745146cb9a5c
Christian7547 <eliasgonzalesec@gmail.com>
2025-05-10 19:32:45 -0400

    saleController exception

--- src/main/java/com/javierito/javierito_importer/infrastructure/controllers/SaleController.java
+++ src/main/java/com/javierito/javierito_importer/infrastructure/controllers/SaleController.java
@@ -5,6 +5,8 @@ import com.javierito.javierito_importer.application.Services.interfaces.ISaleSer
 import com.javierito.javierito_importer.domain.models.SaleModels.SaleReport;
 import com.javierito.javierito_importer.domain.models.SaleModels.SalesDetails;
 import com.javierito.javierito_importer.infrastructure.dtos.sale.SaleParamsDTO;
+import com.javierito.javierito_importer.infrastructure.exception.types.BadRequestException;
+import com.javierito.javierito_importer.infrastructure.exception.types.ResourceNotFoundException;
 import org.springframework.format.annotation.DateTimeFormat;
 import com.javierito.javierito_importer.domain.models.SaleModels.Sale;
 import com.javierito.javierito_importer.infrastructure.dtos.sale.SaleDTO;
@@ -30,16 +32,20 @@ public class SaleController {
                                       @RequestParam(defaultValue = "1")int offset,
                                       @RequestBody SaleParamsDTO options){
         var data = saleService.getAll(limit, offset, options.getInitDate(), options.getFinishDate(), options.getParams());
+        if(data.isEmpty()) {
+            throw new ResourceNotFoundException("sales");
+        }
         return new ResponseEntity<>(data, HttpStatus.OK);
     }
 
     @PostMapping("/newSale")
     public ResponseEntity<?> newSale(@RequestBody SaleDTO saleDTO) throws JsonProcessingException {
         Sale toSale = saleMapper.toSale(saleDTO);
-        if(saleService.createSale(toSale) > 0)
-            return new ResponseEntity<>(HttpStatus.CREATED);
-        else
-            return new ResponseEntity<>(HttpStatus.BAD_REQUEST);
+        if(saleService.createSale(toSale) <= 0) {
+            throw new BadRequestException("could not create the sale");
+        }
+        return new ResponseEntity<>(HttpStatus.CREATED);
+
     }
 
     @GetMapping("/SaleReports")
@@ -55,27 +61,27 @@ public class SaleController {
         List<SaleReport> salesDetails = saleService.getSalesReport(from, to);
 
         if (salesDetails.isEmpty()) {
-            return new ResponseEntity<>(HttpStatus.NO_CONTENT);
+            throw new ResourceNotFoundException("reports");
         }
-
         return new ResponseEntity<>(salesDetails, HttpStatus.OK);
     }
 
     @PatchMapping("/deleteSale/{id}")
     public ResponseEntity<?> deleteSale(@PathVariable long id, @RequestParam int newStatus){
         boolean success = saleService.deleteSale(id, (short) newStatus);
-        if(success)
-            return new ResponseEntity<>(HttpStatus.NO_CONTENT);
-        return new ResponseEntity<>(HttpStatus.NOT_FOUND);
+        if(!success) {
+            throw new ResourceNotFoundException("sale", "id", Long.toString(id));
+        }
+        return new ResponseEntity<>(HttpStatus.NO_CONTENT);
     }
 
     @DeleteMapping("/refund/{saleId}")
     public ResponseEntity<?> refund(@PathVariable long saleId) {
         try {
             saleService.refund(saleId);
-            return new ResponseEntity<>("Successful refund", HttpStatus.OK);
+            return new ResponseEntity<>(HttpStatus.NO_CONTENT);
         } catch (Exception e){
-            throw new RuntimeException(e.getMessage());
+            throw new BadRequestException(e.getMessage());
         }
     }
 }
