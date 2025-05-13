7cc0bc25629c2e8a404b417fe6c4a77cd50afd26
Furinyan <101646097+Furinyan-Gi@users.noreply.github.com>
2025-05-12 20:43:18 -0400

    add insertReport

--- src/main/java/com/javierito/javierito_importer/application/Services/interfaces/IReportService.java
+++ src/main/java/com/javierito/javierito_importer/application/Services/interfaces/IReportService.java
@@ -1,5 +1,6 @@
 package com.javierito.javierito_importer.application.Services.interfaces;
 
+import com.javierito.javierito_importer.domain.models.InsertReport;
 import com.javierito.javierito_importer.domain.models.Report;
 
 import java.sql.Timestamp;
@@ -9,4 +10,5 @@ public interface IReportService {
 
     List<Report> getReports(int limit, int offset, String param, Timestamp startDate, Timestamp endDate, String order);
 
+    int insertReport(InsertReport report);
 }
